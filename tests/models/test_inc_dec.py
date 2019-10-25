b61cd41982cdf8e46736ea6f9e06d7bcf1379ead
DavAug <Dav.Aug@github.com>
2019-10-25 17:36:04 +0100

    init 3

--- tests/models/test_inc_dec.py
+++ tests/models/test_inc_dec.py
@@ -1,4 +1,4 @@
-import src.models.inc_dec as inc_dec    # The code to test
+import PKPD.models.inc_dec as inc_dec    # The code to test
 
 def test_increment():
     assert inc_dec.increment(3) == 4
