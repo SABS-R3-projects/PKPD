e3c46c5f7680c1892cabd69a158891e841394afc
DavAug <david.augustin@gmx.net>
2020-02-04 15:51:37 +0000

    init multi output 2 comp subcut data generation

---
+++ demo/synthesised_two_compartment/generate_multi_output_data.py
@@ -0,0 +1,29 @@
+"""Generates time-series drug concentration data in central compartment based on .2comp_subct_linear.mmt and saves it to .csv file.
+"""
+import numpy as np
+import pandas as pd
+
+from PKPD.model import model as m
+from PKPD.inference import inference
+
+# generating data
+file_name = 'demo/synthesised_two_compartment/2comp_subcut_linear.mmt'
+model = m.MultiOutputModel(file_name)
+
+# List of parameters: ['centralCompartment.drug', 'doseCompartment.drug', 'peripheralCompartment.drug', 'centralCompartment.CL',
+# 'centralCompartment.Kcp', 'centralCompartment.V', 'doseCompartment.Ka', 'peripheralCompartment.Kpc', 'peripheralCompartment.V']
+true_parameters = [0, 0, 0, 1, 3, 5, 2, 2, 2]
+
+times = np.linspace(0.0, 24.0, 100)
+model_result = model.simulate(true_parameters, times)
+
+# add white noise to generate data
+scale = np.mean(model_result) * 0.05 # arbitrary choice of noise (not too much, not too little)
+data = model_result + np.random.normal(loc=0.0,
+                                       scale=scale,
+                                       size=len(model_result)
+)
+
+df = pd.DataFrame({'time_h': times, 'concentration_ng_mL': data})
+df.to_csv('demo/synthesised_two_compartment/two_compartment_multi_output.csv', index=False)
+
