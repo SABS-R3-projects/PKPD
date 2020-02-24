73bdd9953f11d0008e8c8aa61b45cd9c1e98a8c2
Simon Marchant <simon.marchant@linacre.ox.ac.uk>
2020-02-24 13:53:24 +0000

    add requirements to setup.py instead

--- setup.py
+++ setup.py
@@ -7,47 +7,6 @@ from setuptools.command.build_py import build_py
 with open('README.md') as f:
     readme = f.read()
 
-class BuildPyCommand(build_py):
-  """Custom build command."""
-
-  def run(self):
-    self.run_command('pints')
-    build_py.run(self)
-
-
-class PINTS_installer(Command):
-    """A costum command to install PINTS in the process of setting up the PKPD module.
-    """
-    description = ('Clones and pip installs PINTS.')
-
-    def initialize_options(self) -> None:
-        self.commands = []
-
-    def finalize_options(self) -> None:
-        self.commands = [
-            'git clone https://github.com/pints-team/pints.git',
-            'pip install pints/']
-
-    def run(self):
-        """Run commands.
-        """
-        git_clone_cmd = self.commands[0]
-        pip_install_cmd = self.commands[1]
-
-        # clone PINTS repository.
-        if os.path.isdir('pints'):
-            self.announce(
-                'PINTS repository exists in directory',
-                level=distutils.log.INFO)
-        elif not os.path.isfile('./pints'):
-            self.announce(
-                'Running command: %s' % str(git_clone_cmd),
-                level=distutils.log.INFO)
-            os.system(git_clone_cmd)
-
-        # pip install PINTS
-        os.system(pip_install_cmd)
-
 # Go!
 setup(
     # Module name (lowercase)
@@ -66,12 +25,6 @@ setup(
     maintainer_email='david.augustin@dtc.ox.ac.uk',
     url='https://github.com/SABS-R3-projects/PKPD.git',
 
-    # customised commands
-    cmdclass={
-        'pints': PINTS_installer,
-        'build_py': BuildPyCommand
-    },
-
     # Packages to include
     packages=find_packages(include=('PKPD','PKPD.*')),
 
@@ -85,5 +38,8 @@ setup(
         'matplotlib>=1.5',
         'myokit>=1.29',
         'tabulate',
+    ],
+    dependency_links=[
+     "git+git://github.com/pints-team/pints.git#egg=pints",
     ]
 )
